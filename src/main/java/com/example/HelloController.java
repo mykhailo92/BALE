2c662969cfb02f18586a1be2738edea184ed86fb
Florian Altergott <f.altergott2@web.de>
2022-09-21 22:14:27 +0200

    - Hinzufügen eines Beenden Knopfes (Temporär, damit die App beendet werden kann) - fxmlStyle.css hinzugefügt, weil mehrere Buttons existieren - Paar Kommentare hinzugefügt

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,29 +10,40 @@ import netscape.javascript.JSObject;
 import java.io.File;
 
 public class HelloController {
-    public Button getSections;
+    @FXML
+    public Button nextButton;
+    @FXML
+    public Button closeButton;
     @FXML
     private WebView learningUnit = new WebView();
     WebEngine engine;
     File file = new File(String.valueOf(getClass().getResource("/com/example/example.html")));
-    @FXML
-    Button changeColor;
 
+    /**
+     * Starte die Webengine und lade das Beispiel
+     */
     @FXML
     private void initialize() {
         engine = learningUnit.getEngine();
         engine.load(file.toString());
     }
 
+    /**
+     * Executes JavaScript to get and prepare a List of all Sections in the Learningunit
+     * @return List of all Section in the current Learningunit in order of appierence
+     */
     @FXML
-    private void getSections() {
+    private JSObject getSections() {
         JSObject sectionList = (JSObject) engine.executeScript("getSections();");
         System.out.println(sectionList.getSlot(1));
+        return sectionList;
     }
 
+    /**
+     * Temporary fix to close the App in Fullscreenmode
+     */
     @FXML
-    private void changeColorFunction(ActionEvent event) {
-        Object b = engine.executeScript("changeBgColor();");
-        System.out.println(b.getClass());
+    private void closeApp() {
+        Platform.exit();
     }
 }
