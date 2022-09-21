466d69a9457b51db88c44e4a53e402510237749b
Florian Altergott <f.altergott2@web.de>
2022-09-21 17:27:41 +0200

    Fullscreen (temporär)

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -11,7 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 960, 720);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setFullScreen(true);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
