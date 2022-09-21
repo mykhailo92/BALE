a54f81b17729ce7c37465be4e979ed8cc79bc790
Florian Altergott <f.altergott2@web.de>
2022-09-21 19:47:10 +0200

    Mit JSObject

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,22 +3,20 @@ package com.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-
+import netscape.javascript.JSObject;
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class HelloController {
     public Button getSections;
     @FXML
-    private WebView learningUnit =new WebView();
+    private WebView learningUnit = new WebView();
     WebEngine engine;
     File file = new File(String.valueOf(getClass().getResource("/com/example/example.html")));
     @FXML
     Button changeColor;
+
     @FXML
     private void initialize() {
         engine = learningUnit.getEngine();
@@ -27,11 +25,13 @@ public class HelloController {
 
     @FXML
     private void getSections() {
-        Object b = engine.executeScript("getSections();");
+        JSObject b = (JSObject) engine.executeScript("getSections();");
         System.out.println(b.getClass());
     }
+
     @FXML
     private void changeColorFunction(ActionEvent event) {
-        engine.executeScript("changeBgColor();");
+        Object b = engine.executeScript("changeBgColor();");
+        System.out.println(b.getClass());
     }
 }
