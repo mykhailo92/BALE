cad1006d223c8eb484fff40dfbc3af30b836de95
mykhailo <192mike192@gmail.com>
2022-12-02 03:17:07 +0100

    Drop-Down v1

---
+++ Lerneinheit/js/dropDown.js
@@ -0,0 +1,21 @@
+
+const dropdown = document.querySelectorAll('.dropdown');
+dropdown.forEach(function (item) {
+    item.addEventListener('click', function () {
+        item.classList.toggle("active")
+    })
+});
+
+function show(value, index) {
+    if (value === 'right') {
+        switch (index) {
+            case 1: document.getElementById("text-box-" + index).value = '——————————————————>'; break
+            case 2: document.getElementById("text-box-" + index).value = '——————————————————>';
+        }
+    } else if (value === 'left') {
+        switch (index) {
+            case 1: document.getElementById("text-box-" + index).value = '<——————————————————'; break
+            case 2: document.getElementById("text-box-" + index).value = '<——————————————————';
+        }
+    }
+}
