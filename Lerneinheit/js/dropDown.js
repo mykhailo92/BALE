181ccd4275d45e00eaf62cd2b0836af98d7d2b52
mykhailo <192mike192@gmail.com>
2022-12-06 16:41:28 +0100

    Drop-Down v2.1

--- Lerneinheit/js/dropDown.js
+++ Lerneinheit/js/dropDown.js
@@ -8,13 +8,9 @@ dropdown.forEach(function (item) {
 
 function show(value, index) {
     if (value === 'right') {
-        let img = document.createElement('img');
-        img.src = 'icons/arrow-right.png';
         document.getElementById("text-box-" + index)
             .setAttribute('src','icons/arrow-right.png' );
     } else if (value === 'left') {
-        let img = document.createElement('img');
-        img.src = 'icons/arrow-left.png';
         document.getElementById("text-box-" + index)
             .setAttribute('src','icons/arrow-left.png' );
     }
