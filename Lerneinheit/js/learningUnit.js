d9e061e2aaf89ff363290892e78f5ee8580637bf
uol <mykhailo.ievreinov@uni-oldenburg.de>
2023-03-23 22:16:21 +0100

    error fix v2

--- Lerneinheit/js/learningUnit.js
+++ Lerneinheit/js/learningUnit.js
@@ -105,17 +105,9 @@ function showVideo(video) {
       });
 
     /* When the user clicks on <span> (x), close the modal */
-    span.onclick = function() {
+      span.onclick = function() {
         modal.style.display = "none";
         window.javaBridge.saveFeedback("Demo-video",getDateTime(),0,"Stop playing");
-    }
-
-    /* When the user clicks anywhere outside the modal, close it */
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-            window.javaBridge.saveFeedback("Demo-video",getDateTime(),0,"Stop playing");
-        }
     }
 }
 
